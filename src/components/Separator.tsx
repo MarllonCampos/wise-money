cafb15f4b120b51c7b99c2478b8faae7f8b91d27
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-02-22 03:50:29 -0300

    feat: create a componente for separator

---
+++ src/components/Separator.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, StyleSheet, ViewProps } from "react-native";
+import Colors from "../../Colors";
+
+// import { Container } from './styles';
+interface Props extends ViewProps {}
+export function Separator(props: Props) {
+  return <View {...props} style={styles.separator} />;
+}
+
+const styles = StyleSheet.create({
+  separator: {
+    maxWidth: 1,
+    flex: 1,
+    marginRight: 10,
+    marginLeft: 10,
+    height: "95%",
+    backgroundColor: Colors.gray[100],
+  },
+});
