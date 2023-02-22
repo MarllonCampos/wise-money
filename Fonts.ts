ade134bf21693f1ad0e72a8d2a1089b38b740d30
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-02-22 03:50:04 -0300

    feat: add default styles for fonts

--- Fonts.ts
+++ Fonts.ts
@@ -1,4 +1,6 @@
-export default {
+import { StyleSheet } from "react-native"
+import Colors from "./Colors"
+const Fonts = {
     Manrope: {
         medium: "Manrope_500Medium",
         bold: "Manrope_700Bold",
@@ -9,3 +11,43 @@ export default {
         medium: "Roboto_500Medium",
     }
 }
+const styles = StyleSheet.create({
+    manrope: {
+        color: Colors.blue,
+        fontFamily: Fonts.Manrope.bold,
+    },
+    manropeH1: {
+        fontSize: 18,
+    },
+    manropeH2: {
+        fontSize: 14,
+    },
+    manropeH3: {
+        fontSize: 12,
+    },
+    roboto: {
+        fontFamily: Fonts.Roboto.regular,
+        color: Colors.gray[600],
+    },
+    robotoH1: {
+        fontSize: 14,
+    },
+    robotoH2: {
+        fontSize: 12,
+    },
+})
+export default {
+    Manrope: {
+        medium: "Manrope_500Medium",
+        bold: "Manrope_700Bold",
+        extraBold: "Manrope_800ExtraBold",
+    },
+    Roboto: {
+        regular: "Roboto_400Regular",
+        medium: "Roboto_500Medium",
+    },
+    styles
+}
+
+
+
